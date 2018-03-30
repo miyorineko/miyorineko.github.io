f97a820db4c7c17b49c22c0057582a6ace212070
miyorineko <miyorineko@users.noreply.github.com>
2018-03-30 19:34:21 +0800

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -10,3 +10,25 @@ else
 	clear_install
 	exit 1
 fi
+
+clear
+vmesslink="vmess://"$(echo -e "{
+  \"ps\": \"${hostname}\",
+  \"add\": \"${Address}\",
+  \"port\": \"${install_port}\",
+  \"id\": \"${UUID}\",
+  \"aid\": \"100\",
+  \"net\": \"tcp\",
+  \"type\": \"none\",
+  \"host\": \"\",
+  \"tls\": \"\"
+  }" | base64)
+echo -e "您的连接信息如下："
+echo -e "别名(Remarks)：${hostname}"
+echo -e "地址(Address)：${Address}"
+echo -e "端口(Port)：${install_port}"
+echo -e "用户ID(ID)：${UUID}"
+echo -e "额外ID(AlterID)：100"
+echo -e "加密方式(Security)：aes-128-gcm"
+echo -e "传输协议(Network）：tcp"
+echo -e "Vmess链接：${green_backgroundcolor}${vmesslink}${default_fontcolor}"
