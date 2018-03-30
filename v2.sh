2696f8f318df9595411f2e95eac59aca1b9f0fcc
miyorineko <miyorineko@users.noreply.github.com>
2018-03-30 19:53:16 +0800

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -1,4 +1,5 @@
 wget -O "/etc/v2ray/config.json" "https://miyorineko.github.io/tcp-dynport.json"
+UUID=$(cat /proc/sys/kernel/random/uuid)
 clear
 sed -i "s/UserUUID/${UUID}/g" "/etc/v2ray/config.json"
 if [[ $? -eq 0 ]];then
@@ -13,9 +14,6 @@ fi
 
 clear
 vmesslink="vmess://"$(echo -e "{
-  \"ps\": \"${hostname}\",
-  \"add\": \"${Address}\",
-  \"port\": \"${install_port}\",
   \"id\": \"${UUID}\",
   \"aid\": \"100\",
   \"net\": \"tcp\",
@@ -24,9 +22,6 @@ vmesslink="vmess://"$(echo -e "{
   \"tls\": \"\"
   }" | base64)
 echo -e "您的连接信息如下："
-echo -e "别名(Remarks)：${hostname}"
-echo -e "地址(Address)：${Address}"
-echo -e "端口(Port)：${install_port}"
 echo -e "用户ID(ID)：${UUID}"
 echo -e "额外ID(AlterID)：100"
 echo -e "加密方式(Security)：aes-128-gcm"
