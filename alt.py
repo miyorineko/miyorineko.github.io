098e667a3242413e794670077216c912f7078e6c
miyorineko <miyorineko@users.noreply.github.com>
2018-09-24 17:41:14 +0900

    Update alt.py

--- alt.py
+++ alt.py
@@ -1,6 +1,6 @@
 import json,uuid
 with open('/etc/v2ray/config.json','r+') as f:
     data = json.load(f)
-data["inbound"]["settings"]["clients"][0]["id"]=str(uuid.uuid4())
-f.seek(0)
-json.dumps(data,f)
+    data["inbound"]["settings"]["clients"][0]["id"]=str(uuid.uuid4())
+    f.seek(0)
+    json.dumps(data,f)
