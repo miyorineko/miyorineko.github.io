ae10cc058241e708bbf511322e06c3070c1d7040
miyorineko <miyorineko@users.noreply.github.com>
2018-09-24 17:38:36 +0900

    Update alt.py

--- alt.py
+++ alt.py
@@ -1,5 +1,5 @@
 import json,uuid
-with open('/etc/v2ray/config.json','rw') as f:
+with open('/etc/v2ray/config.json','r+') as f:
     data = json.load(f)
 data["inbound"]["settings"]["clients"][0]["id"]=str(uuid.uuid4())
 json.dumps(data,f)
